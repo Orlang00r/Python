40df271b59b4d30436815e1c32d36665940c5357
orlangur <orlangur00r@yandex.ru>
2024-03-23 10:26:46 +0400

    Python 1

---
+++ main.py
@@ -0,0 +1,22 @@
+
+# n = 384916
+# l = n//1000; suml=0
+# r = n-l*1000;sumr=0
+# while l>0:
+#     suml+=l%10; l = l//10
+#     sumr+=r%10; r = r//10
+# if suml==sumr:
+#     print("yes")
+# else:
+#     print ("no")
+
+# a = 2
+# b = 6
+# c = 10
+# if a*b<c or c==a*b:
+#     print ('no')
+# elif c%a==0 or c%b==0:
+#     print ('yes')
+# else: 
+#     print ('no')
+
